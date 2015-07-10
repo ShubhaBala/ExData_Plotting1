48a8f8a5f692668a5b85e2724bb13ad12adcb34f
Shubha Bala <shubha.bala@gmail.com>
2015-07-10 12:05:30 -0400

    Fixed second plot

--- gettingdata.R
+++ gettingdata.R
@@ -2,4 +2,5 @@ library(sqldf)
 data <- read.csv.sql("household_power_consumption.txt", 
                       sql = "select * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'", sep=";",eol = "\n")
 data["DateTime"]<- paste(data[,1], data[,2])
-strptime(data[,10], "%d/%m/%Y %H:%M:%S")
+library(lubridate)
+data[,10]<-dmy_hms(data[,10])
--- plot2.R
+++ plot2.R
@@ -1,3 +1,3 @@
-plot(data[,"Global_active_power"], type="l", ylab="Global Active Power (kilowatts)", xlab="", xaxt = "n")
-x=c(0, length(data[,"Global_active_power"])/2, length(data[,"Global_active_power"]))
+plot(data[,10],data[,"Global_active_power"], type="l", ylab="Global Active Power (kilowatts)", xlab="", xaxt = "n")
+x=c(data[1,10], data[length(data[,10])/2, 10], data[length(data[,10]),10])
 axis(1, at=x, labels=c("Thu","Fri","Sat"))
